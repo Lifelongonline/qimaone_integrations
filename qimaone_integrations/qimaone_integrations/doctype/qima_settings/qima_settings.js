9f38d1ffe811df90dec41d6af8ff93ac8d69f3db
Gopal8848 <gopal@8848digital.com>
2026-04-01 12:20:59 +0530

    feat: Qimone Integrations

---
+++ qimaone_integrations/qimaone_integrations/doctype/qima_settings/qima_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, gopal@8848digital.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Qima Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ qimaone_integrations/qimaone_integrations/doctype/qima_settings/test_qima_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, gopal@8848digital.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQimaSettings(FrappeTestCase):
+	pass
