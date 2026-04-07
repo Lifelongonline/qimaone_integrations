9f238fa38642d07c311971698ddcbe5345066981
rohitkumar8848 <rohitkumar@8848digital.com>
2026-04-07 16:33:10 +0530

    feat: QIMA integrations

--- qimaone_integrations/qimaone_integrations/doctype/qima_settings/qima_settings.js
+++ qimaone_integrations/qimaone_integrations/doctype/qima_settings/qima_settings.js
@@ -1,8 +1,57 @@
 // Copyright (c) 2026, gopal@8848digital.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Qima Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Qima Settings", {
+	refresh(frm) {
+		frm.events.create_refresh_buttons(frm);
+		frm.events.create_po_button(frm);
+		frm.events.download_inspections_button(frm);
+	},
+	create_refresh_buttons(frm) {
+		frm.add_custom_button(__("Generate Refresh Token"), function () {
+			if (!frm.doc.email_id || !frm.doc.password || !frm.doc.api_token) {
+				frappe.msgprint(
+					__(
+						"Please fill in Email ID, Password, and API Token before generating the refresh token."
+					)
+				);
+				return;
+			}
+			frappe.call({
+				method: "qimaone_integrations.qimaone_integrations.doctype.qima_settings.api.api.generate_refresh_token",
+				args: {
+					doc: frm.doc,
+				},
+				callback: function (r) {
+					const msg = r.message;
+					if (msg) {
+						frm.set_value("refresh_token", msg);
+						frm.save().then(() => {
+							frappe.msgprint(__("Refresh token generated and saved successfully."));
+						});
+					}
+				},
+			});
+		});
+	},
+	create_po_button(frm) {
+		frm.add_custom_button(__("Create PO in QIMAone"), function () {
+			if (!frm.doc.default_qima_uom) {
+				frappe.throw(
+					__(frappe.throw("Please set the Default QIMA UOM before creating the PO."))
+				);
+			}
+			window.open(
+				"/api/method/qimaone_integrations.qimaone_integrations.doctype.qima_settings.api.api.append_draft_inspections_to_csv",
+				"_blank"
+			);
+			// frappe.call({
+			//     method: "qimaone_integrations.qimaone_integrations.doctype.qima_settings.api.api.append_draft_inspections_to_csv",
+			//     freeze: true
+			// })
+		});
+	},
+	download_inspections_button(frm) {
+		frm.add_custom_button(__("Get Inspection Report from Qimaone"), function () {});
+	},
+});
